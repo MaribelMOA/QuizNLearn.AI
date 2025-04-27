40299b5c62d64a37bad353104a92bda81a0de444
MaribelMOA <maribel.mendez@uabc.edu.mx>
2025-04-26 18:22:53 -0700

    Ya sea creany guardan quizzes con ia

--- resources/views/quizzes/_quiz-card.blade.php
+++ resources/views/quizzes/_quiz-card.blade.php
@@ -112,7 +112,6 @@
 
         <div class="flex items-center space-x-2">
 
-
             <a  id="start-quiz-{{ $questionnaire->id }}"  href="" class="inline-flex items-center px-3 py-1 bg-indigo-600 text-white text-sm rounded-md hover:bg-indigo-700 transition-colors">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
