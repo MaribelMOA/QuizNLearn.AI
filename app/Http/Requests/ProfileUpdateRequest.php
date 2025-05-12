423c7a3df6ba90096ae7d3ca2906c7dce7e71dd8
MaribelMOA <maribel.mendez@uabc.edu.mx>
2025-05-11 22:00:47 -0700

    App/models/Plan en Profiel Update

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
-use App\models\Plan;
+use App\Models\Plan;
 
 class ProfileUpdateRequest extends FormRequest
 {
